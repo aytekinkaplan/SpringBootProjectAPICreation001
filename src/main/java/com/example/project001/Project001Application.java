c4a5fe51b54ea58c79049cf00ed58c08a344e4f1
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-09-30 15:38:01 +0300

    coomitted

--- src/main/java/com/example/project001/Project001Application.java
+++ src/main/java/com/example/project001/Project001Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Project001Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Project001Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Project001Application.class, args);
+    }
 
 }
